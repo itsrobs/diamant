779707ae8a151f8bf561a72b7910e988e376be9f
Robert Oppedal <rob@torshov.biz>
2024-09-17 19:21:49 +0200

    Added userinputs and interactions, and crazy color mode

--- diamant2.py
+++ diamant2.py
@@ -2,39 +2,36 @@ import turtle as t
 from math import sqrt
 from random import randint
 
-def firkant(antall):
+
+def firkant(antall=5, colorS="n", lengde=20, vinkel=90, sider=4):
 
     def tegn(sider):
         for side in range(sider):
             t.forward(xLengde)
             t.right(vinkel)
 
-    
-    lengde = 20
     xLengde = lengde*antall
-    vinkel = 90
-    sider = 4
     diagonal = sqrt((lengde**2)+(lengde**2))
-    counter = 1
+    counter = antall
+    counter2 = 0
     
     color1 = "black"
     color2 = "white"
     
-    colorS = "r"
-
-    t.Screen().bgcolor("black")
+    t.Screen().bgcolor("brown")
     t.speed(0)
 
     t.setx(-((diagonal/2)*antall))
 
     t.left(45)
     for mengde in range(antall):
-        if colorS.lower() == "r":
+        if colorS.lower() == "y":
             t.colormode(255)
             t.fillcolor(randint(1,255), randint(1,255), randint(1,255))
+            t.Screen().bgcolor(randint(1,255), randint(1,255), randint(1,255))
         else:
             t.colormode(1)
-            if counter % 2 == 0:
+            if counter % 2 != 0:
                 t. fillcolor(color1)
             else:
                 t.fillcolor(color2)
@@ -46,12 +43,28 @@ def firkant(antall):
         t.forward(diagonal/2)
         t.left(45)
         xLengde -= lengde
-        counter += 1
+        counter -= 1
+        counter2 += 1
         t.pendown()
-    t.exitonclick()
+    t.done()
 
 
+def main():
+    def tall_input(beskjed):
+        while True:
+            try:
+                tall = int(input(beskjed))
+                return tall
+            except ValueError:
+                print("Skriv et gyldig tall...")
+            except KeyboardInterrupt:
+                print("\nPeace out!")
+                exit()
+    antall_firkanter = tall_input("Hvor mange diamanter?: ")
+    lengde = tall_input("Størrelse: ")
+    fargevalg = input("Crazy farge modus? (y/N): ")
+    firkant(antall_firkanter,colorS=fargevalg, lengde=lengde)
 
 
 if __name__=="__main__":
-    firkant(50)
+    main()
